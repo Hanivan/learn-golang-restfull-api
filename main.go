e94c1fb2ed24c4d00bcb048fad54f74520cb89b7
アニップ <Hanivan@users.noreply.github.com>
2024-02-02 13:08:00 +0700

    chore: done restful api

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"Hanivan/learn-golang-restfull-api/app"
 	"Hanivan/learn-golang-restfull-api/controller"
-	"Hanivan/learn-golang-restfull-api/exception"
 	"Hanivan/learn-golang-restfull-api/helper"
 	"Hanivan/learn-golang-restfull-api/middleware"
 	"Hanivan/learn-golang-restfull-api/repository"
@@ -13,7 +12,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -22,16 +20,7 @@ func main() {
 	categoryRepository := repository.NewCategoryRepository()
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
-
-	router := httprouter.New()
-
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
